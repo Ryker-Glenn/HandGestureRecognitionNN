50cb00d612e96f935e1da04d440752c0415c56ea
Ryker Frohock <frohockr@yahoo.com>
2020-11-23 01:49:44 -0500

    Redid the motion history update and still has a lot of work to do

--- Gesture.cpp
+++ Gesture.cpp
@@ -3,10 +3,11 @@
 Gesture::Gesture() {}
 
 void Gesture::capture() {
-	Mat frame, fgMask, fgBlur, fgThresh;
+	Mat frame, fgMask, fgBlur, fgThresh, merged_frame;
 	Ptr<BackgroundSubtractor> pBackSub;
-	pBackSub = createBackgroundSubtractorMOG2();			// trained NN from OpenCV used to remove everything not moving in the background
-	cap.open(0);												// Open webcam
+	pBackSub = createBackgroundSubtractorMOG2(false);			// trained NN from OpenCV used to remove everything not moving in the background
+	cap.open("gesture_tests/swipe_right.mp4");
+	/*cap.open(0);*/ // Open webcam
 	while (true) {
 		cap >> frame;
 		if (frame.empty()) { break; }
@@ -16,42 +17,108 @@ void Gesture::capture() {
 		for (int i = 1; i < MAX_KERNEL_LENGTH; i += 2)
 			medianBlur(fgMask, fgBlur, i);						// For the kernel size, apply opencv's median blur function to frame with removed
 																// background
-		threshold(fgMask, fgThresh, 0, MAX_BV, BI);				// apply binary inverted threshold to the blurred frame
-		imshow("BI Blur FG Mask", fgThresh);
+		threshold(fgBlur, fgThresh, 0, MAX_BV, BI);				// apply binary inverted threshold to the blurred frame
+		/*imshow("BI Blur FG Mask", fgThresh);*/
+		update_mhi(fgThresh, merged_frame);
+
+		imshow("mhi", merged_frame);
 		// show video frame-by-frame
 		//get the input from the keyboard
-		int keyboard = waitKey(30);
-		if (keyboard == 'q' || keyboard == 27)
-			break;
+		if (waitKey(10) >= 0)
+            break;
 	}
-
 }
 
-Mat Gesture::modulo(Mat img, int mod) {
-	Mat after_mod = img.clone();
-	for (int y = 0; y < img.rows; y++) {
-		for (int x = 0; x < img.cols; x++) {
-			after_mod.at<uchar>(y, x) = after_mod.at<uchar>(y, x) % mod;
+void Gesture::update_mhi(const Mat& img, Mat& dst) {
+	if (dst.size() != img.size()) {
+		dst = Mat::zeros(img.size(), CV_8U);
+		dst.setTo(255);
+	}
+	else {
+		Mat after_mod, progress_set, init_frame(img);
+		double similarity;
+		update_progress(img, progress_set);
+		mod(img, after_mod);
+		add(after_mod, dst);
+		similarity = structural_similarity(init_frame, dst);
+		if (abs(similarity - prev_sim) > THRESH1) {
+			prog++;
+		}
+		prev_sim = similarity;
+	}
+}
+
+void Gesture::mod(const Mat& img, Mat& after) {
+	after.create(img.rows, img.cols, img.type());
+	for (int y = 0; y < img.cols * img.rows; y++) {
+		after.at<uchar>(y) = img.at<uchar>(y) % MOD;
+	}
+}
+
+void Gesture::add(const Mat& mod, const Mat& merged) {
+	Mat src1(mod), src2(merged);
+}
+
+double Gesture::structural_similarity(Mat& init, Mat& merged) {
+	int px_init = 0, px_merged = 0, total = 0;
+	double avg_init, avg_merged;
+	long variance_init, variance_merged, covar;
+	for (int y = 0; y < init.cols * init.rows; y++) {
+		total++;
+		px_init += init.at<uchar>(y);
+		px_merged += merged.at<uchar>(y);
+	}
+	avg_init = average(px_init, total);
+	avg_merged = average(px_merged, total);
+	variance_init = variance(init, avg_init, total);
+	variance_merged = variance(merged, avg_merged, total);
+	covar = covariance(init, merged, avg_init, avg_merged, total);
+
+	return ssim(avg_init, avg_merged, covar, variance_init, variance_merged);
+}
+
+void Gesture::update_progress(const Mat& img, Mat& progress) {
+	const int nChannels = img.channels();
+	progress = Mat::zeros(img.size(), CV_8U);
+	progress.setTo(255);
+	int ncols = img.rows * img.cols;
+	for (int i = 0; i < ncols; i++) {
+		if (img.at<uchar>(i) == 0) {
+			progress.at<uchar>(i) = prog;
 		}
 	}
-	return after_mod;
 }
 
-void Gesture::update_mhi(Mat img) {
-	Mat after_mod, mf;
-	for (int y = 0; y < img.rows; y++) {
-		for (int x = 0; x < img.cols; x++) {
-			if (img.at<uchar>(y, x) == 0) {
-				img.at<uchar>(y, x) = progress;
-			}
-		}
+double Gesture::average(int colors, int pixels) {
+	return colors / pixels;
+}
+
+long Gesture::variance(const Mat& img, double avg, int ct) {
+	long sum = 0;
+	for (int y = 0; y < img.cols * img.rows; y++) {
+		sum += square(img.at<uchar>(y) - avg);
 	}
-
-	after_mod = modulo(img, 255);
-	mf = add(after_mod, img);
+	return sum / (long)ct;
 }
 
-Mat Gesture::add(Mat after, Mat merged) {
-	addWeighted(after, 0.5, merged, 0.5, 0.0, merged);
-	return merged;
+long Gesture::covariance(const Mat& init, const Mat& merged, double avg_init, double avg_merged, int ct) {
+	long sum = 0;
+	for (int y = 0; y < init.cols * init.rows; y++) {
+		sum += (init.at<uchar>(y) - avg_init) * (merged.at<uchar>(y) - avg_merged);
+		
+	}
+	return sum / ((long)ct - 1);
+}
+
+long Gesture::square(long diff) {
+	return diff * diff;
+}
+
+long Gesture::ssim(long avg_x, long avg_y, long covar, long var_x, long var_y) {
+	long tl = (2 * avg_x * avg_y) + C1;
+	long tr = (2 * covar) + C2;
+	long bl = square(avg_x) + square(avg_y) + C1;
+	long br = var_x + var_y + C2;
+
+	return (tl * tr) / (bl * br);
 }
--- Gesture.h
+++ Gesture.h
@@ -6,24 +6,43 @@
 #include <opencv2/objdetect.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/video.hpp>
+#include <opencv2/optflow.hpp>
 
-#define DELAY_BLUR 100
-#define MAX_KERNEL_LENGTH 11
-#define MAX_BV 255
-#define BI 1
+#define DELAY_BLUR			100
+#define MAX_KERNEL_LENGTH	11
+#define MAX_BV				255
+#define BI					1
+#define MOD					255
+#define THRESH1				0
+#define THRESH2				0
+#define C1					6.5025		// Calculated as (k_1 * L)^2 where k_1 = 0.01 and L is 2^8 - 1
+#define C2					58.5225		// Calculated as (k_2 * L)^2 where k_2 = 0.03 and L is 2^8 - 1
 
 using namespace std;
 using namespace cv;
+using namespace cv::motempl;
 
 class Gesture {
 public:
-	int progress = 0;
-	Mat merged_frame, init_frame;
+	int prog = 0;
+	double prev_sim = 0;
+	const double MHI_DURATION = 5;
+	const double MAX_TIME_DELTA = 0.5;
+	const double MIN_TIME_DELTA = 0.05;
 	VideoCapture cap;
+	vector<Mat> buf; // img ring buffer
 	Gesture();
 	void capture();
 private:
-	Mat modulo(Mat, int);
-	void update_mhi(Mat);
-	Mat add(Mat, Mat);
+	void update_mhi(const Mat&, Mat&);
+	void update_progress(const Mat&, Mat&);
+	void mod(const Mat&, Mat&);
+	void add(const Mat&, const Mat&);
+	double structural_similarity(Mat&, Mat&);
+
+	double average(int, int);
+	long variance(const Mat&, double, int);
+	long covariance(const Mat&, const Mat&, double, double, int);
+	long square(long);
+	long ssim(long, long, long, long, long);
 };
