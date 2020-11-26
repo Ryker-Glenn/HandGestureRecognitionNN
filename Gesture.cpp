2adeec14185c2b506c42f9118cc2da348e0444ee
Ryker Frohock <frohockr@yahoo.com>
2020-11-26 02:06:26 -0500

    Updating comments on structural similarity functions and removing some irrelevant variables

--- Gesture.cpp
+++ Gesture.cpp
@@ -5,83 +5,104 @@ Gesture::Gesture() {}
 void Gesture::capture() {
 	Mat frame, fgMask, fgBlur, fgThresh, merged_frame;
 	Ptr<BackgroundSubtractor> pBackSub;
-	pBackSub = createBackgroundSubtractorKNN(false);			// trained NN from OpenCV used to remove everything not moving in the background
-	cap.open("gesture_tests/test2.mp4");
-	//cap.open(0); 
+	pBackSub = createBackgroundSubtractorMOG2();
+	cap.open("gesture_tests/b.mp4"); 
+	//cap.open(0);
 	while (true) {
 		cap >> frame;
 		if (frame.empty()) { break; }
-		pBackSub->apply(frame, fgMask);							// apply background remover to current video
-		rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
-			cv::Scalar(255, 255, 255), -1);
+		pBackSub->apply(frame, fgMask);
 		for (int i = 1; i < MAX_KERNEL_LENGTH; i += 2)
-			medianBlur(fgMask, fgBlur, i);						// For the kernel size, apply opencv's median blur function to frame with removed
-																// background
-		threshold(fgBlur, fgThresh, 0, MAX_BV, BI);				// apply binary inverted threshold to the blurred frame
-		/*imshow("BI Blur FG Mask", fgThresh);*/
-		//update_mhi(fgThresh, merged_frame);
+			medianBlur(fgMask, fgBlur, i);						
 
+		threshold(fgBlur, fgThresh, 0, MAX_COLOR, THRESH_BINARY_INV | THRESH_OTSU);
+		update_mhi(fgThresh, merged_frame);
 		imshow("mhi", fgThresh);
-		// show video frame-by-frame
-		//get the input from the keyboard
 		if (waitKey(10) >= 0)
             break;
 	}
 	//imwrite("detected_gesture/swipe_left.png", merged_frame);
 }
 
-void Gesture::update_mhi(const Mat& img, Mat& dst) {
+void Gesture::update_mhi(Mat& img, Mat& dst) {
 	if (dst.size() != img.size()) {
-		dst = Mat::zeros(img.size(), CV_8U);
-		dst.setTo(255);
+		dst = img.clone();
 	}
 	else {
-		Mat after_mod, progress_set, init_frame(img);
+		Mat after_mod, init_frame(img);
 		double similarity;
-		update_progress(img, progress_set);
-		mod(img, after_mod);
-		dst = add(after_mod, progress_set, dst);	// after_mod is the all black img, dst should be the merged
-		similarity = Similarity::ssim(init_frame, dst);
-		if (abs(similarity - prev_sim) > THRESH1) {
-			prog += 8;
-		}
+		update_progress(img);
+		after_mod = mod(img);
+		add(after_mod, dst);	
+		similarity = ssim(init_frame, dst);
+		if (abs(similarity - prev_sim) > THRESH1)
+			prog += 5;
+
 		prev_sim = similarity;
 	}
 }
 
-void Gesture::mod(const Mat& img, Mat& after) {
-	after.create(img.rows, img.cols, img.type());
-	for (int y = 0; y < img.cols * img.rows; y++) {
-		after.at<uchar>(y) = img.at<uchar>(y) % MOD;
-	}
+Mat Gesture::mod(const Mat& img) {
+	Mat after(img);
+	for (int y = 0; y < img.cols * img.rows; y++) 
+		after.at<uchar>(y) = img.at<uchar>(y) % MAX_COLOR;
+
+	return after;
 }
 
-Mat Gesture::add(Mat& modded, Mat& progress, Mat& merged) {
-	Mat dst = Mat::zeros(modded.size(), CV_8U);
-	dst.setTo(255);
-	uchar p, o, op;
+void Gesture::add(Mat& modded, Mat& merged) {
 	for (int i = 0; i < modded.cols * modded.rows; i++) {
-		p = modded.at<uchar>(i);
-		o = progress.at<uchar>(i);
-		op = merged.at<uchar>(i);
-		if (o != 255) {
-			dst.at<uchar>(i) = o;
-		}
-		if (op != 255) {
-			dst.at<uchar>(i) = op;
-		}
+		if (merged.at<uchar>(i) != modded.at<uchar>(i))
+			merged.at<uchar>(i) = modded.at<uchar>(i);
 	}
-	return dst;
 }
 
-void Gesture::update_progress(const Mat& img, Mat& progress) {
-	const int nChannels = img.channels();
-	progress = Mat::zeros(img.size(), CV_8U);
-	progress.setTo(255);
-	int ncols = img.rows * img.cols;
-	for (int i = 0; i < ncols; i++) {
-		if (img.at<uchar>(i) == 0) {
-			progress.at<uchar>(i) = prog;
-		}
+void Gesture::update_progress(Mat& img) {
+	for (int i = 0; i < img.rows * img.cols; i++) {
+			img.at<uchar>(i) = (img.at<uchar>(i) == 0) ? prog : img.at<uchar>(i);
 	}
+}
+
+double Gesture::avg(int px, int count) { return (double)(px / count); }
+double Gesture::sq(double to_sq) { return to_sq * to_sq; }
+
+double Gesture::variance(const Mat& img, double avg, int total) {
+	long sum = 0;
+	for (int y = 0; y < img.cols * img.rows; y++) 
+		sum += sq(img.at<uchar>(y) - avg);
+	
+	return sum / total;
+}
+
+double Gesture::covariance(const Mat& init, const Mat& merged, double avg_init, double avg_merged, int ct) {
+	long sum = 0;
+	for (int y = 0; y < init.cols * init.rows; y++) 
+		sum += (init.at<uchar>(y) - avg_init) * (merged.at<uchar>(y) - avg_merged);
+	
+	return sum / ct;
+}
+
+double Gesture::ssim(const Mat& img, const Mat& merged) {
+	int px_init = 0, px_merged = 0, total = 0;
+	double avg_img, avg_merged, v_img, v_merged, covar;
+	for (int y = 0; y < img.cols * img.rows; y++) {
+		total++;
+		px_init += img.at<uchar>(y);
+		px_merged += merged.at<uchar>(y);
+	}
+	avg_img = avg(px_init, total);
+	avg_merged = avg(px_merged, total);
+	v_img = variance(img, avg_img, total);
+	v_merged = variance(merged, avg_merged, total);
+	covar = covariance(img, merged, avg_img, avg_merged, total);
+	return calc_ssim(avg_img, avg_merged, v_img, v_merged, covar);
+}
+
+double Gesture::calc_ssim(double avx, double avy, double vx, double vy, double cv) {
+	double tl = (2 * avx * avy) + C1;	// tl and bl are top/bottom left of structural similarity eqxn
+	double bl = sq(avx) + sq(avy) + C1;	// which calculates luminance
+	double tr = (2 * cv) + C2;			// while top/bottom right calculate contrast
+	double br = vx + vy + C2;			// this equation allows calculation of structural simularity
+
+	return (tl * tr) / (bl * br);
 }
--- Gesture.h
+++ Gesture.h
@@ -6,15 +6,12 @@
 #include <opencv2/objdetect.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/video.hpp>
-#include "Similarity.h"
 
-#define DELAY_BLUR			100
 #define MAX_KERNEL_LENGTH	11
-#define MAX_BV				255
-#define BI					1
-#define MOD					255
-#define THRESH1				0.5
-#define THRESH2				0
+#define MAX_COLOR			255
+#define THRESH1				0.3
+#define C1					6.5025
+#define C2					58.5225
 
 using namespace std;
 using namespace cv;
@@ -27,8 +24,25 @@ public:
 	Gesture();
 	void capture();
 private:
-	void update_mhi(const Mat&, Mat&);
-	void update_progress(const Mat&, Mat&);
-	void mod(const Mat&, Mat&);
-	Mat add(Mat&, Mat&, Mat&);
+	void update_mhi(Mat&, Mat&);
+	void update_progress(Mat&);
+	Mat mod(const Mat&);		
+	void add(Mat&, Mat&);
+
+	/* utility functions used to determine structural 
+	similarity between initial frame and merged frame */
+	double avg(int, int);
+	double sq(double);
+
+	// calculates expectation of a pixel from its mean
+	double variance(const Mat&, double, int);
+
+	// calculates expected value of the product of x/y's deviations from their individual means
+	double covariance(const Mat&, const Mat&, double, double, int);
+
+	// Structural similarity calculations between an initial image and the merged image
+	double ssim(const Mat&, const Mat&);
+
+	// does the actual important calculations
+	double calc_ssim(double, double, double, double, double);
 };
